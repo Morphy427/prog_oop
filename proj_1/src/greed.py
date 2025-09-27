a023690eb4d85d230d80473e9901b4babe72f903
Morphy427 <pubelm@laposte.net>
2025-09-27 14:10:44 +0200

    💩 i don't have brain anymore

---
+++ proj_1/src/greed.py
@@ -0,0 +1,46 @@
+import recup_go as rg
+import compute_score as cs
+
+## initiation : 
+summary = {} # type set temporaire, reste a definir
+candidates = rg.getOverRepresentedAnnot() # un set d'EOI over-represented (a voir si recup dans rg ou si nouveau script pre-traitement.py)
+eltAnnotatedByCandidates = rg.getEOIannotated()
+eltAnnotatedBySummary = {}
+
+
+def findBest():
+    # 
+    #
+    #
+    return
+
+def addToSummary():
+    #
+    #
+    #
+    return
+
+def discardFromCandidates():
+    #
+    #
+    #
+    return
+
+def checkIfAnnotate():
+    #
+    #
+    #
+    return
+
+def candidatesEmpty():
+    # return if the set candidates is empty
+    #
+    #
+    return
+
+def doGreedy():
+    #
+    #
+    #
+    
+    return
